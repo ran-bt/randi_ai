1a25bda825012c24b66af5603ef78c399b11285f
Inran <inranb@gmail.com>
2023-01-04 23:10:52 -0700

    server url updated

--- clint/script.js
+++ clint/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data or bot's response from server
-  const response = await fetch("https://randi-ai.onrender.com", {
+  const response = await fetch("https://randi-ai-vvnw.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
